6fcf389e4228dff4af410c55d87af67164dc8d79
Anusha Lihala <alihala@liveperson.com>
2024-06-23 21:21:55 +0530

    Update join game component and integrate with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DieComponent } from './die/die.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DieComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'sushi-on-the-roll';
-  hello() {
-    console.log('hello');
-  }
 }
--- src/app/join-game/join-game.component.ts
+++ src/app/join-game/join-game.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GameService } from '../../game.service';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Unsubscribe } from 'firebase/firestore';
 
 @Component({
   selector: 'app-join-game',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, FormsModule],
   templateUrl: './join-game.component.html',
-  styleUrl: './join-game.component.css'
+  styleUrl: './join-game.component.css',
 })
 export class JoinGameComponent {
+  gameIdInput: string = '';
+  gameService = inject(GameService);
+  // unsubscribeFromGame: Unsubscribe;
 
+  constructor(private snackBar: MatSnackBar) {
+    // this.unsubscribeFromGame = this.gameService.trackGame((gameSnap) =>
+    //   console.log(gameSnap)
+    // );
+  }
+
+  // ngOnDestroy(): void {
+  //   this.unsubscribeFromGame();
+  // }
+
+  joinGame() {
+    this.gameService.joinGame(this.gameIdInput).then((result) => {
+      if (result === true) {
+        this.snackBar.open('Successfully joined game', 'OK', {
+          duration: 2000,
+        });
+      } else {
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      }
+    });
+  }
+
+  createGame() {
+    this.gameService.createGame().then((result) => {
+      if (result === true) {
+        this.snackBar.open('Successfully created game', 'OK', {
+          duration: 2000,
+        });
+      } else {
+        this.snackBar.open('Failed to create game', 'OK', {
+          duration: 2000,
+        });
+      }
+    });
+  }
 }
